678fabb9bcb60adc84f67cdeb16d9eb7780e5914
Gabriel Pereyra <pereyrarg11@gmail.com>
2023-05-30 13:27:32 -0600

    chore: set up dependency injection

---
+++ app/src/main/java/com/pereyrarg11/cryptocurrencyapp/CoinApplication.kt
@@ -0,0 +1,8 @@
+package com.pereyrarg11.cryptocurrencyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication : Application() {
+}
---
+++ app/src/main/java/com/pereyrarg11/cryptocurrencyapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.pereyrarg11.cryptocurrencyapp.di
+
+import com.pereyrarg11.cryptocurrencyapp.common.Constants
+import com.pereyrarg11.cryptocurrencyapp.data.remote.CoinPaprikaApi
+import com.pereyrarg11.cryptocurrencyapp.data.repository.CoinRepositoryImpl
+import com.pereyrarg11.cryptocurrencyapp.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
