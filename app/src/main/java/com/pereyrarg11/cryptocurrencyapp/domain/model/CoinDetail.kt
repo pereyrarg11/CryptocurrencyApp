18579e497808fe28d0fbeae3d688e83e2cde8566
Gabriel Pereyra <pereyrarg11@gmail.com>
2023-05-29 21:33:03 -0600

    chore: implementing API and models

---
+++ app/src/main/java/com/pereyrarg11/cryptocurrencyapp/domain/model/CoinDetail.kt
@@ -0,0 +1,14 @@
+package com.pereyrarg11.cryptocurrencyapp.domain.model
+
+import com.pereyrarg11.cryptocurrencyapp.data.remote.dto.TeamMember
+
+data class CoinDetail(
+    val coinId: String,
+    val name: String,
+    val description: String,
+    val symbol: String,
+    val rank: Int,
+    val isActive: Boolean,
+    val tags: List<String>,
+    val team: List<TeamMember>,
+)
