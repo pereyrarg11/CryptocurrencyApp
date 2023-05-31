e9326de53718cb629991ad32d0e89ddd69294704
Gabriel Pereyra <pereyrarg11@gmail.com>
2023-05-30 21:24:11 -0600

    chore: coin detail screen

--- app/src/main/java/com/pereyrarg11/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/pereyrarg11/cryptocurrencyapp/presentation/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.pereyrarg11.cryptocurrencyapp.presentation.coin_detail.CoinDetailScreen
+import com.pereyrarg11.cryptocurrencyapp.presentation.coin_list.CoinListScreen
 import com.pereyrarg11.cryptocurrencyapp.presentation.ui.theme.CryptocurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +26,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(route = Screen.CoinListScreen.route) {
+                            CoinListScreen(navController = navController)
+                        }
+
+                        composable(route = Screen.CoinDetailScreen.route + "/{coinId}") {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptocurrencyAppTheme {
-        Greeting("Android")
     }
 }
